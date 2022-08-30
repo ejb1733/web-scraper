7ba0456b34ebcfdd07d9603f179737a401a2a9af
ejb1733 <ethanjbillings@gmail.com>
2022-08-29 18:12:05 -0700

    Add seatsummary(n, delay)
    - writes the seat summary n times with delay seconds between each summary

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+import time
 
 driver = webdriver.Chrome()
 driver.implicitly_wait(5)
 driver.get('https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-section&dept=CPSC&course=304&section=2W1')
 
-seat_info = driver.find_element(By.XPATH, '/html/body/div[2]/div[4]/table[4]').text
+def seatsummary(n, delay):
+    for i in range(n):
+        seat_info = driver.find_element(By.XPATH, '/html/body/div[2]/div[4]/table[4]').text
 
-f = open('scraped.txt', 'a')
-f.write(f'\n{seat_info}\n---')
-f.close()
+        f = open('scraped.txt', 'a')
+        f.write(f'\n{seat_info}\n---')
+        f.close()
+        print(seat_info)
 
-f = open('scraped.txt', 'r')
-print(f.read())
+        time.sleep(delay)
+
+seatsummary(3, 5)
