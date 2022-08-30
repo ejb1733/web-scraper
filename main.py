fdffd66f92c981bb9d3d326e88052ab17ebef042
ejb1733 <ethanjbillings@gmail.com>
2022-08-29 17:40:01 -0700

    Add script to login to UBC SSC

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+from logininfo import username, password
+
 driver = webdriver.Chrome()
-driver.get("https://ejb1733.github.io/landing-page/")
-driver.implicitly_wait(30)
-useeeer = driver.find_element(By.CLASS_NAME, "button4")
-useeeer.click()
+driver.get("https://ssc.adm.ubc.ca/sscportal/")
+driver.implicitly_wait(5)
+
+driver.find_element(By.XPATH, '//a[@href="/sscportal/servlets/SRVSSCFramework"]').click()
+
+username_box = driver.find_element(By.ID, 'username')
+password_box = driver.find_element(By.ID, 'password')
+submit_box = driver.find_element(By.CLASS_NAME, 'btn-submit')
+
+username_box.send_keys(username)
+password_box.send_keys(password)
+submit_box.click()
