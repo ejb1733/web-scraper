db722e4deb48d8587d5d436ee0ea149ef49ec2f9
ejb1733 <ethanjbillings@gmail.com>
2022-08-29 18:37:13 -0700

    Add time.ctime() to write message
    - shows the time when the page was last checked

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ driver = webdriver.Chrome()
 driver.implicitly_wait(5)
 driver.get('https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-section&dept=CPSC&course=304&section=2W1')
 
+course_name = driver.find_element(By.TAG_NAME, 'h4').text
+
 def seatsummary(n, delay):
     for i in range(n):
-        seat_info = driver.find_element(By.XPATH, '/html/body/div[2]/div[4]/table[4]').text
+        seat_info = driver.find_element(By.XPATH, '/html/body/div[2]/div[4]/table[4]/tbody').text
 
+        to_print = f'\nSeat Summary for [{course_name}]\n@ {time.ctime()} @\n{seat_info}\n---'
         f = open('scraped.txt', 'a')
-        f.write(f'\n{seat_info}\n---')
+        f.write(to_print)
         f.close()
-        print(seat_info)
+        print(to_print)
 
         time.sleep(delay)
 
