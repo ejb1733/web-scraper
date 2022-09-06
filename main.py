aa120bf39ea8371582fa1da72aa8141b64d6ea77
ejb1733 <ethanjbillings@gmail.com>
2022-09-05 18:03:37 -0700

    Add more descriptive print msgs

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from emailing import sendMail
 import time
 import linecache
 
-COURSE_SITE = 'https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-section&dept=CPSC&course=304&section=2W1'
+COURSE_SITE = 'https://courses.students.ubc.ca/cs/courseschedule?pname=subjarea&tname=subj-section&dept=MATH&course=312&section=101'
 SEAT_SUMMARY_XPATH = '/html/body/div[2]/div[4]/table[4]/tbody'
 CURR_NUM_REGISTERED_XPATH = '/html/body/div[2]/div[4]/table[4]/tbody/tr[2]/td[2]/strong'
 
@@ -16,7 +16,7 @@ course_name = driver.find_element(By.TAG_NAME, 'h4').text
 
 def seatsummary(n, delay):
     last_num_saved = getCurrRegisteredFromFile()
-    print(last_num_saved)
+    print(f'{last_num_saved} (last num from file)')
     for i in range(n):
         seat_summ = driver.find_element(By.XPATH, SEAT_SUMMARY_XPATH).text
         next_num_saved = int(driver.find_element(By.XPATH, CURR_NUM_REGISTERED_XPATH).text)
@@ -32,7 +32,7 @@ def seatsummary(n, delay):
         elif (last_num_saved > next_num_saved):
             sendMail(course_name, 'dropped', last_num_saved, next_num_saved, to_write)
         last_num_saved = next_num_saved
-        print(last_num_saved)
+        print(f'{last_num_saved} @@ {curr_write_time}')
 
         driver.refresh()
         time.sleep(delay)
@@ -55,4 +55,4 @@ def getCurrRegisteredFromFile():
 #     print(last_time)
 #     return last_time
 
-seatsummary(500, 120)
+seatsummary(1000, 30)
